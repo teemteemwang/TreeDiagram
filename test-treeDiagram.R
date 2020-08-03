842cc0c0406f71485216f42e85df3eb0839d2e71
teemteemwang <50971110+teemteemwang@users.noreply.github.com>
2020-08-02 22:15:57 -0700

    Delete test-treeDiagram.R

--- test-treeDiagram.R
+++
@@ -1,4 +0,0 @@
-test_that("An automatically generated tree diagram in user's working directory without returning any error messages", {
-  result = treeDiagram()
-  expect_equal(result$S, "Hello, World!") #zero error message and the whole generation process is non-stop
-})
